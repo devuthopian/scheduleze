925e2cd1b613324d4a2fc07944cd47c50ff490d2
mukesh.kumar <bishnois1@gmail.com>
2018-07-18 19:14:03 +0530

    minor changes

--- resources/views/layouts/includes/front/header.blade.php
+++ resources/views/layouts/includes/front/header.blade.php
@@ -1,34 +1,12 @@
 <div class="head_top_bar">
   <div class="container">
    <ul>
+     <li><a href="{{ url('register') }}">Signup</a></li>
+     <li><a href="#">Newsletter</a></li>
+   </ul>
+  </div>
+</div>
 
-    @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Clinet Login') }}</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('register') }}">{{ __('Signup') }}</a>
-                            </li>
-                             <li><a href="#">Newsletter</a></li>
-                        @else
-                            <li class="nav-item dropdown">
-                                 <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-                                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                        {{ Auth::user()->name }} <span class="caret"></span>
-                                    </a>
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                
-                            </li>
-                        @endguest
-                     </ul>
-                    </div>
-                  </div>
 <div class="header_section">
   <div class="container">
       <div class="head_left">
@@ -38,6 +16,7 @@
       </div>
       <div class="head_right">
          <div class="navigation">
+            
                @guest
                       <nav> 
                       <a href="#" class="n_toggle"><i class="fa fa-bars fa-2x"></i></a>
@@ -145,7 +124,41 @@
                       </ul>         
                       </nav>
                         @endguest
+            <nav> 
+              <a href="#" class="n_toggle"><i class="fa fa-bars fa-2x"></i></a>
+              <ul>
+                <li><a href="{{ url('scheduling_solutions') }}">Scheduling Solutions</a></li>
+                <li><a href="{{ url('success_stories') }}">Success  Stories</a></li>
+                <li><a href="{{ url('demo') }}">Demo</a></li>
+                <li><a href="{{ url('faq') }}">FAQ</a></li>
+                <li>
+                    <a href="{{ route('buildingtypes') }}">{{ __('nav.services') }}</a>
+                    <ul class="dropdown-menu">
+                        <li>
+                            <a href="{{ route('buildingtypes') }}">{{ __('nav.buildtype') }}</a>
+                        </li>
+                        <li>
+                            <a href="{{ route('buildingsizes') }}">{{ __('nav.buildsizes') }}</a>
+                        </li>
+                        <li>
+                            <a href="{{ route('storebuildtype') }}">{{ __('nav.buildages') }}</a>
+                        </li>
+                    </ul>
+                </li>
+                <li><a href="{{ url('contact') }}">Contact</a></li>
+                <li><a href="#">Client Login</a></li>         
+              </ul>         
+            </nav>
         </div>
     </div>
   </div>
 </div>
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
