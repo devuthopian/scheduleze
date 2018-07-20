61b4bdfb3d93ca57a298071698de0734f34a3410
devuthopian <devuthopian@gmail.com>
2018-07-20 10:52:44 +0530

    Mayank's commit

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -55,7 +55,7 @@ class LoginController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function redirectToProvider()
+    public function socialLogin()
     {
         return Socialite::driver('facebook')->redirect();
     }
